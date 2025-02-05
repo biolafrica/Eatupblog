e03e6551c39df2612970d3782516ec2b89cdd1ac
Abiodun Biobaku <biolafrica@gmail.com>
2025-02-05 16:21:26 +0000

    optimize about page

--- public/script.js
+++ public/script.js
@@ -44,6 +44,28 @@
 
   });
 
+  document.addEventListener("DOMContentLoaded", function () {
+    const sections = document.querySelectorAll(
+      ".picture-intro, .who-we-are, .mission, .vision, .come-work"
+    );
+
+    const observer = new IntersectionObserver(
+      (entries, observer) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("section-visible");
+                observer.unobserve(entry.target); 
+            }
+        });
+      },
+      
+      { threshold: 0.2 }
+    );
+
+    sections.forEach((section) => observer.observe(section));
+});
+
+
 
 }());
 
